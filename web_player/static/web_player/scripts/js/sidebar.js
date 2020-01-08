971d9b34b9a8a7c10fb64ee75b78321c02feb8b4
Pavel Antsypovich <pavelantsypovich@gmail.com>
2020-01-08 18:26:24 +0300

    finished fullscreen design

--- web_player/static/web_player/scripts/js/sidebar.js
+++ web_player/static/web_player/scripts/js/sidebar.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
     const sidebar = $(".sidebar");
     const cont= $(".content");
-    const button = $(".bt_sidebar");
+    const button = $(".bt-sidebar");
     hiden = true;
     
     button.click(function() {
         if (hiden) {
-            sidebar.css({"width": "200px"});
-            button.css({"margin-left": "200px"})
-            cont.css({"margin-left": "200px"})
+            sidebar.css({"width": "300px"});
+            button.css({"margin-left": "252px"})
+            button.text("X");
+            cont.css({"margin-left": "300px"})
+            sidebar.css({"opacity": "0.75"})
             hiden = false;
         }
         else {
             sidebar.css({"width": "0"});
             button.css({"margin-left": "0"})
+            button.text("Play list");
             cont.css({"margin-left": "0"})
+            sidebar.css({"opacity": "0"})
             hiden = true;
         }
     });
