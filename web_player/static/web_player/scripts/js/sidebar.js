34fdb904fbf1e049ab6177f89d4257eaf9b4a2ae
Pavel Antsypovich <pavelantsypovich@gmail.com>
2020-01-08 10:35:52 +0300

    -

--- web_player/static/web_player/scripts/js/sidebar.js
+++ web_player/static/web_player/scripts/js/sidebar.js
@@ -2,16 +2,18 @@ $(document).ready(function() {
     const sidebar = $(".sidebar");
     const cont= $(".content");
     const button = $(".bt_sidebar");
-    hiden = false;
+    hiden = true;
     
     button.click(function() {
         if (hiden) {
             sidebar.css({"width": "200px"});
+            button.css({"margin-left": "200px"})
             cont.css({"margin-left": "200px"})
             hiden = false;
         }
         else {
             sidebar.css({"width": "0"});
+            button.css({"margin-left": "0"})
             cont.css({"margin-left": "0"})
             hiden = true;
         }
